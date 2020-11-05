d483ea445491bd8b470da5a6e5d8ea54efe098e0
ChristianAlmariego <65950078+ChristianAlmariego@users.noreply.github.com>
2020-11-05 18:09:38 +0800

    commit resources 2

---
+++ application/views/Employee_Services_files/script_009.js
@@ -0,0 +1,10 @@
+(function(props,context) {
+        Core.utils.renderBootstrap({
+          elId:'bs-5',
+          componentName:'PopupModal',
+          props:props,
+          context:context,
+          contextKey:'context-bs-4',
+          radpack:false
+        });
+      })({"upgradeable":false,"preset":"popup1","id":"9a6e0b69-4403-4110-b4dc-ca45792ba1f1","websiteId":"0aa640bc-3bff-41d5-83fb-228636f1ac29","lastPublished":"2020-10-23T13:46:45.499Z","enabled":false,"popupTitle":"Announcement","popupDescription":"{\"blocks\":[{\"key\":\"dj5kn\",\"text\":\"Welcome! Check out my new announcement.\",\"type\":\"unstyled\",\"depth\":0,\"inlineStyleRanges\":[],\"entityRanges\":[],\"data\":{}}],\"entityMap\":{}}","cta":{"enabled":true,"label":"Learn more","pageId":"2dbc3c68-54c1-4511-8f4b-14af5ec7c5f0","linkType":"internal","linkId":"ca826457-884c-48cd-b29e-9c420cfbaedf"},"popupImage":{"width":"","left":"","height":"","shrinkFit":false,"position":"","alt":"","editedAspectRatio":"","top":"","video":"","image":"","rotation":""},"widgetId":"9a6e0b69-4403-4110-b4dc-ca45792ba1f1","section":"default","category":"neutral","locale":"en-PH","renderMode":"PUBLISH"},{"widgetId":"9a6e0b69-4403-4110-b4dc-ca45792ba1f1","widgetType":"POPUP","widgetPreset":"popup1","section":"default","category":"neutral","fontSize":"medium","fontFamily":"alternate","websiteThemeOverrides":{},"widgetThemeOverrides":{}});
